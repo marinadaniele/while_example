ae55b00a3bdbb83446e1042525f83d4020a64a0f
marinadaniele <marinadanielee@gmail.com>
2024-01-24 18:14:40 +0100

    Initial commit

---
+++ while_example.py
@@ -0,0 +1,19 @@
+
+import random
+lives = 3
+
+while lives:
+    print("You have", lives, "lives left")
+    dice_roll = random.randint(1,6)
+    if dice_roll == 6:
+        print("You rolled a 6, you win!!!")
+        break
+    else:
+        print("You rolled a", dice_roll, "try again")
+        lives = lives - 1 #or "lives -=1"
+
+if lives == 0:
+    print("You lost all your lives, game over")
+
+# else
+#   print("You lost all your lives, game over")
